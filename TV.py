137f70646c3fb8280c317871741dc7e15f4290a5
gerikah <aldaygerikah@gmail.com>
2023-05-15 00:39:29 +0800

    def update

--- TV.py
+++ TV.py
@@ -1,1 +1,8 @@
 # this will contain the code for the TV class
+
+class TV :
+    def __init__(self, channel, volume_level, tv_on):
+        # set default values for channel, volume and tv on
+        self.channel = 1
+        self.volume_level = 1
+        self_tv_on = False
