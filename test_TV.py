70442f76e980b0463705a316f6822b0a61cd5909
gerikah <aldaygerikah@gmail.com>
2023-05-17 00:23:06 +0800

    rearranged the lines

--- test_TV.py
+++ test_TV.py
@@ -1,15 +1,16 @@
 # this file will contain the test driver program that creates two objects from TV class
 
+# import module with exception handling
 try:
     import pyfiglet
 except ImportError:
     print("The required module 'pyfiglet' is not installed.\nPlease install the module to continue.")
 
+# for design
 border = "_"*180
 program_title = ("TV Tester")
 
-print("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format(program_title, justify = "center", font = "isometric1", width = 175) + "\n" + border)
-
+# import from TV class
 from TV import TV
 def main ():
     # object 1: tv1
@@ -18,7 +19,7 @@ def main ():
     tv_1.set_channel(30)
     tv_1.set_volume_level(3)
     test_tv1 = ("TV 1's channel is", tv_1.get_channel(), "and volume level is ", tv_1.get_volume_level())
-    print(test_tv1)
+    centered_tv1 = ' '.join(str(item) for item in test_tv1).center(180)
 
     # object 2: tv2
     tv_2 = TV()
@@ -26,7 +27,13 @@ def main ():
     tv_2.set_channel(3)
     tv_2.set_volume_level(2)
     test_tv2 = ("TV 2's channel is", tv_2.get_channel(), "and volume level is ", tv_2.get_volume_level())
-    print(test_tv2)
+    centered_tv2 = ' '.join(str(item) for item in test_tv1).center(180)
+
+    # print all the results
+    print("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format(program_title, justify = "center", font = "isometric1", width = 175) + "\n" + border)
+    print("\n\n" + centered_tv1)
+    print("\n\n" + centered_tv2)
+    print("\n" + border)
 
 if __name__ == "__main__":
     main()
